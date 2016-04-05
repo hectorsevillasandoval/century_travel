06486d6d39df4c7c15deaa243670eac21ab8827b
Hector Sevilla <h2d_07@hotmail.com>
2016-04-04 21:39:23 -0600

    Agregada configuracion base de gulp con PostCSS

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp'),
+  gutil = require('gulp-util'),
+  webserver = require('gulp-webserver'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  precss = require('precss'),
+  cssnano = require('cssnano');
+
+
+  var config = {
+  	"source" : './dev/',
+  	"dest" : './public/'
+
+  }
+
+  	gulp.task('html', function() {
+  		gulp.src(config.source + '*.html')
+  		.pipe(gulp.dest(config.dest));
+	});
+
+// PostCSS
+  gulp.task('css', function() {
+  gulp.src(config.source +'css/'+ 'main.css')
+  .pipe(postcss([
+    precss(),
+    autoprefixer(),
+    cssnano()
+  ]))
+  .on('error', gutil.log)
+  .pipe(gulp.dest(config.dest + 'css'));
+});
+
+  gulp.task('watch', function() {
+  gulp.watch(config.source + 'css/*.css', ['css']);
+  gulp.watch(config.source + '*.html', ['html']);
+});
+
+gulp.task('webserver', function() {
+  gulp.src(config.dest)
+    .pipe(webserver({
+      livereload: true,
+      open: true
+    }));
+});
+
+gulp.task('default', ['html', 'css', 'webserver','watch']);
